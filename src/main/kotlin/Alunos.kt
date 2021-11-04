9d3c08a2980435b37b2d4582e485176b7ba4a9c0
LucasCaetanos <87659302+LucasCaetanos@users.noreply.github.com>
2021-11-03 23:43:48 -0300

    Atualizando

--- src/main/kotlin/Alunos.kt
+++ src/main/kotlin/Alunos.kt
@@ -1,47 +1,54 @@
-class Alunos (var primeiroNome: String?, var ultimoNome: String? = null) {
+class Estudante (var primeiroNome :String?,  var ultimoNome: String?) {
 
     var codigoAluno = 0
     var mediaAluno = 0.0
     var ano = 0
-    val foiAprovado: Boolean
+    val foiAprovado : Boolean
         get(){
-            if (mediaAluno >=60){
+            if (mediaAluno >=60.0) {
                 return true
             }else{
                 return false
             }
         }
 
-    constructor(primeiroNome: String?, ultimoNome: String?, codigoAluno: Int):
+    constructor(primeiroNome: String?, ultimoNome: String?, condigoAluno : Int) :
             this(primeiroNome, ultimoNome){
-                this.codigoAluno = codigoAluno
-            }
-    constructor(primeiroNome: String?, ultimoNome: String?, codigoAluno: Int, mediaAluno: Double, ano : Int):
-            this(primeiroNome, ultimoNome, codigoAluno){
-                this.ano = ano
-                this.mediaAluno = mediaAluno
-            }
+        this.codigoAluno = condigoAluno
+    }
+
+    constructor(primeiroNome: String?, ultimoNome: String?, condigoAluno: Int,
+                mediaAluno: Double, ano : Int )
+            :this(primeiroNome, ultimoNome, condigoAluno){
+        this.ano = ano
+        this.mediaAluno = mediaAluno
+    }
+
 
 
     fun nomeCompleto(){
-        println("$primeiroNome $ultimoNome")
+        println("Nome completo aluno: $primeiroNome $ultimoNome.")
     }
 
-    fun passouDeAno() : Int{
+    fun passouDeAno() : Int {
         if (foiAprovado){
-            ano +=1
+            ano+=1
             println("Parabéns, você passou de ano $ano")
         }else{
             println("Reprovado")
         }
+
         return 0
     }
 
-    fun infoAluno(){
+
+
+    fun mostraInformacoesAluno(){
         println("Nome: $primeiroNome $ultimoNome")
         println("Código: $codigoAluno")
         println("Média: $mediaAluno")
-        println("Próximo ano: $ano")
+        println("Ano: $ano")
+        println("Foi aprovado: $foiAprovado")
     }
 
 
--- src/main/kotlin/Cursos.kt
+++ src/main/kotlin/Cursos.kt
@@ -1,38 +1,54 @@
-class Cursos (var nomeCurso: String?, var serie : Int) {
-    var professor: String? = null
+class Cursos (var nomeCurso : String?, var serie : Int){
+    var professor : String? = null
+    val listaEstudantesMatriculados = mutableListOf<Estudante>()
 
-    val listaAlunos = mutableListOf<Alunos>()
 
-    fun matricularAlunos(aluno: Alunos){
-        listaAlunos.add(aluno)
+    fun matricularEstudante(estudante: Estudante){
+        listaEstudantesMatriculados.add(estudante)
+    }
+
+    @Override
+    fun matricularEstudante(listaDeEstudantes : Array<Estudante>){
+        for (estudante in listaDeEstudantes ){
+            listaEstudantesMatriculados.add(estudante)
+        }
+
+
     }
 
 
-    fun descadastro(aluno: Alunos){
-        println("Você quer remover o cadastro do AlunO?" +
-                "Informe Sim para prosseguir e não para desistir ")
-        aluno.infoAluno()
+
+    fun descadastrarAluno(estudante: Estudante){
+        println("Você realmente deseja remover o estudante abaixo? Digite Sim para" +
+                " remover ou Não para desistir da remoção")
+        estudante.mostraInformacoesAluno()
         var confirmaRemocao = readLine()!!.uppercase()
 
-        if (confirmaRemocao == "SIM"){
-            listaAlunos.remove(aluno)
+        if(confirmaRemocao == "SIM"){
+            listaEstudantesMatriculados.remove(estudante)
+            println("Estudante removido com sucesso")
         }else{
-            println("Cancelado")
+            println("Estudante não removido")
         }
+
+
+
+
     }
 
-    fun quantosAlunos(): Int{
-        return listaAlunos.size
+    fun contaEstudantesCadastrados() : Int{
+        return listaEstudantesMatriculados.size
     }
 
-    fun maiorMedia(): Double{
-        var melhorNota = 0.0
-        for (aluno in listaAlunos){
-            if (aluno.mediaAluno > melhorNota){
-                melhorNota = aluno.mediaAluno
+    fun retornaMaiorMediaDoCurso() : Double{
+        var melhorMedia = 0.0
+
+        for (estudante in listaEstudantesMatriculados){
+            if (estudante.mediaAluno > melhorMedia){
+                melhorMedia = estudante.mediaAluno
             }
         }
-        return melhorNota
+        return melhorMedia
     }
 
 
